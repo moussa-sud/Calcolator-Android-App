eed5bc14f9ff67e5f4b0e3aaed32065d7137aa1f
MOUSSA <moussa.casa16@gmail.com>
2024-03-17 03:03:04 +0300

    yes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -12,6 +13,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -30,10 +32,35 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // For control over item selection of both touch and mouse driven selection
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+
+
+
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
+
+
+
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 }
--- app/src/main/java/com/example/normal/MainActivity.java
+++ app/src/main/java/com/example/normal/MainActivity.java
@@ -1,46 +1,287 @@
 package com.example.normal;
+import static android.content.ContentValues.TAG;
+import static com.google.android.gms.auth.api.signin.GoogleSignInOptions.DEFAULT_SIGN_IN;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button equalBtn ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView RecyclerView = findViewById(R.id.mlistView);
 
+        // in here we are initializing our variables
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        RecyclerView.setLayoutManager(layoutManager);
+        equalBtn = findViewById(R.id.btnResult);
 
-
-
-
-        ArrayList<Person> peopleList = new ArrayList<>();
-
-        for(int i = 0 ; i < 50 ; i++ ){
-
-
-            peopleList.add(new Person( "Moussa TIGUI", "2000", "Male"));
-
-
-        }
+        TextView resultSet = findViewById(R.id.resultsTV);
 
 
 
+        // for the buttons I directly passed their id through the findViewById below
 
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(  peopleList);
+        findViewById(R.id.btn0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        RecyclerView.setAdapter(adapter);
+                resultSet.setText(resultSet.getText().toString()+"0");
+                equalBtn.setEnabled(true);
 
 
-    }
-}
+            }
+        });
+
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"1");
+
+                equalBtn.setEnabled(true);
+
+            }
+        });
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"2");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+
+
+        findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"3");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+
+        findViewById(R.id.btn4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"4");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+
+        findViewById(R.id.btn5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"5");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+
+
+        findViewById(R.id.btn6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"6");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+
+
+        findViewById(R.id.btn7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"7");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+        findViewById(R.id.btn8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"9");
+                equalBtn.setEnabled(true);
+
+            }
+        });
+
+
+        findViewById(R.id.btn9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+"9");
+                equalBtn.setEnabled(true);
+
+
+            }
+        });
+
+
+        findViewById(R.id.bntSub).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+" - ");
+
+            }
+        });
+
+        findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+" + ");
+
+            }
+        });
+
+
+
+        findViewById(R.id.btnDiv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText(resultSet.getText().toString()+" / ");
+
+            }
+        });
+
+        findViewById(R.id.btnDot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  resultSet.setText(resultSet.getText().toString()+".");
+                  equalBtn.setEnabled(true);
+            }
+        });
+
+
+        findViewById(R.id.btnMulti).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                equalBtn.setEnabled(true);
+
+                resultSet.setText(resultSet.getText().toString()+" x ");
+
+            }
+        });
+
+        findViewById(R.id.cleanAll).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resultSet.setText("");
+                equalBtn.setEnabled(false);
+
+
+            }
+        });
+
+
+        findViewById(R.id.btnResult).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(resultSet.getText() == null){
+                    // in here when the button 'Equal' is being pressed and the user doesn't
+                    // enter anything the button will being disable temporary
+                    // till the user enter other number will be activating again
+
+                    findViewById(R.id.btnResult).setEnabled(false);
+                }
+
+
+                // In here I made a variable 'stringNumbers' to store all our data
+                // as String numbers
+
+                String stringNumbers  = resultSet.getText().toString(); //////////<<<<<<<<<<<<<
+
+                // in here I  declared an array of strings
+                // because we want to split the string based on spaces
+
+                String [] splittingNums = stringNumbers.split(" ");
+
+
+                double sum = Double.parseDouble(splittingNums[0]);
+                int NumbersId = 0 ;  // 1 >> 3 >> 5
+
+
+                //  Numbers        2   +   2   +   2   +    2    +    2
+                // stringArray[i]  0  '1'  2  '3'  4  '5'   6   '7'   8
+                //  operator ici her zaman tek olacak, ve sayılar her zaman çift olacak
+
+
+                for (int i = 1; i < splittingNums.length; i += 2) {
+
+
+                    // at the first the for loop will start from the numbers then it moves to the operation
+                    // if the if condition finds any kind of operations (+ , / , * , -)
+                    // it gonna implies the necessary operation
+
+                    if (splittingNums[i].equals("+")){
+                        sum += Double.parseDouble(splittingNums[NumbersId += 2]);
+                    }
+                    else if (splittingNums[i].equals("-")){
+                        sum -= Double.parseDouble(splittingNums[NumbersId += 2]);
+                    }
+                    else if (splittingNums[i].equals("x")){
+                        sum *= Double.parseDouble(splittingNums[NumbersId += 2]);
+                    }
+                    else if (splittingNums[i].equals("/")){
+                        sum /= Double.parseDouble(splittingNums[NumbersId += 2]);
+                    }
+                }
+
+                resultSet.setText(sum+"");
+
+
+
+            }
+        });
+
+
+    }}
